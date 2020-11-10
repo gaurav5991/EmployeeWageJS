6b8d9174dd550ec7d40b315962d783a8c97fcdc6
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-10 10:26:33 +0530

    Gaurav[ADDED]: While Loop to meet given Condition for given Hours and Month Limit

--- NewEmployeeWage.js
+++ NewEmployeeWage.js
@@ -10,12 +10,15 @@ if (empCheck == IS_ABSENT) {
 
 //UC2 Switch case
 //UC 3 Refactor to write a function
+//UC4 Calculate wages for a month
+//UC5 Calculate wage for a month or given Hours Limit
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck) {
   switch (empCheck) {
@@ -32,10 +35,13 @@ function getWorkingHours(empCheck) {
 }
 
 let totalEmpHrs = 0;
-for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
-    let empCheck = Math.floor(Math.random()*10)%3;
+let totalWorkingDays = 0;
+while((totalEmpHrs<=MAX_HRS_IN_MONTH)&&(totalWorkingDays<NUM_OF_WORKING_DAYS)){
+    empCheck = Math.floor(Math.random()*10)%3;
     totalEmpHrs += getWorkingHours(empCheck);
+    totalWorkingDays++;
 }
 
 let empWage = totalEmpHrs*WAGE_PER_HOUR;
-console.log("Total emp hours = "+totalEmpHrs+" Total employee wage = "+empWage);
+console.log("Total working days: "+totalWorkingDays);
+console.log("Total emp hours = "+totalEmpHrs+" Total employee wage = "+empWage)
