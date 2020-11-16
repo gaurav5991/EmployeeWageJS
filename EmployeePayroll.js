3a2aea4ed27f12380ef3c29bf5755618a3540bd3
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-16 22:20:15 +0530

    Gaurav[ADDED]:Regex in name setter and threw an exception in case of wrong name

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,57 +1,67 @@
-class EmployeePayrollData {
-  id;
-  salary;
-  gender;
-  startDate;
+class EmployeePayrollData{
+    startDate;
 
-  //constructor
-  constructor(...params){
-    this.id = params[0];
-    this.name = params[1];
-    this.salary = params[2];
-    this.gender = params[3];
-    this.startDate = params[4];
-}
-  //getter and setter
-  get id(){
-    return this._id;
-}
-set id(id){
-    console.log("setting id :"+id);
-    this._id = id;
-}
-get salary(){
-    return this._salary;
-}
-set salary(salary){
-    this._salary = salary;
-}
-get gender(){
-    return this._gender;
-}
-set gender(gender){
-    this._gender = gender;
-}
-get name(){
-    return this._name;
-}
-set name(name){
-    this._name = name;
-}
+    //constructor
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
 
-  //toString
-  toString() {
-    const options = {year: 'numeric', month: 'long', day:'numeric'};
+    //getter and setter
+    get id(){
+        return this._id;
+    }
+    set id(id){
+        this._id = id;
+    }
+    get salary(){
+        return this._salary;
+    }
+    set salary(salary){
+        this._salary = salary;
+    }
+    get gender(){
+        return this._gender;
+    }
+    set gender(gender){
+        this._gender = gender;
+    }
+    get name(){
+        return this._name;
+    }
+    set name(name){
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if(nameRegex.test(name)){
+          this._name = name;
+        }
+        else throw 'Name is incorrect';
+    }
+
+    //toString
+    toString(){
+        const options = {year: 'numeric', month: 'long', day:'numeric'};
         const empDate = this.startDate == undefined ? "undefined":
                         this.startDate.toLocaleDateString("en-IN",options);
         return "id = "+this.id+",name = "+this.name+",salary = "+this.salary +",gender = "+this.gender+",start date = "+empDate;
     }
 }
 
-let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
+let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
 console.log(employeePayrollData.toString());
-employeePayrollData.name = "john";
-console.log(employeePayrollData.toString());
+try{
+    employeePayrollData.name = "john";
+    console.log(employeePayrollData.toString());
+}
+catch(e){
+    console.error(e);
+}
 
 let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
-console.log(newEmployeePayrollData.toString()); 
+console.log(newEmployeePayrollData.toString());
+
+
+let newEmployeePayrollData2 = new EmployeePayrollData(1,"Terr",30000,"F",new Date());
+console.log(newEmployeePayrollData2.toString());
